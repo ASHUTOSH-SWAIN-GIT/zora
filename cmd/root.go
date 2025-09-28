53b1e02479fb2f8d2f7c90e7c8fe12c11f7558b9
ASHUTOSH-SWAIN-GIT <ashutoshswain7383@gmail.com>
2025-09-29 00:15:04 +0530

    fix

--- cmd/root.go
+++ cmd/root.go
@@ -12,7 +12,7 @@ var rootCmd = &cobra.Command{
 	Use:   "zora",
 	Short: "Zora is a CLI tool to download a specific folder from GitHub.",
 	Long: `A fast and simple command-line tool written in Go to download a
-specific folder from a public GitHub repository without cloning the entire project.`,
+	specific folder from a public GitHub repository without cloning the entire project.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		ascii := figlet4go.NewAsciiRender()
 
@@ -23,11 +23,6 @@ specific folder from a public GitHub repository without cloning the entire proje
 
 		fmt.Println("A fast and simple tool to download specific folders from GitHub.")
 		fmt.Println("-----------------------------------------------------------------")
-		fmt.Println("\nUsage:")
-		fmt.Println("  zora download <github-folder-url> [flags]")
-		fmt.Println("\nExample:")
-		fmt.Println("  zora download https://github.com/spf13/cobra/tree/main/docs")
-		fmt.Println("\nRun 'zora download --help' for more information.")
 	},
 }
 
